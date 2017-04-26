908757b04543389b1cc5cf048b1796481b442a85
Andrii Volin <angy.volin@gmail.com>
2017-04-26 20:24:51 +0300

    add test enviroment

---
+++ tests/ODMServiceProviderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tequila\Pimple\Provider\Tests;
+
+use Tequila\MongoDB\Tests\Functional\TestCase;
+
+class ODMServiceProviderTest extends TestCase
+{
+    public function testOptionsRegistered()
+    {
+        $this->markTestIncomplete();
+    }
+}
